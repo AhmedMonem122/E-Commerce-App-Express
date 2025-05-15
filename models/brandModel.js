2f930366b39795c4535ced8a896f516b382b950f
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-05-15 22:03:04 +0300

    feat: add some properties to brand model

--- models/brandModel.js
+++ models/brandModel.js
@@ -6,8 +6,28 @@ const brandSchema = new mongoose.Schema({
     required: [true, "Please provide a brand title!"],
     minlength: [3, "A title should have at least 3 minimum characters!"],
   },
+  description: {
+    type: String,
+    required: [true, "Please provide a brand description!"],
+    minlength: [
+      10,
+      "A description should have at least 10 minimum characters!",
+    ],
+  },
+  category: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Category",
+    required: [true, "Please provide a brand category!"],
+  },
+  products: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+    },
+  ],
   image: {
     type: String,
+    required: [true, "Please provide a brand image!"],
   },
   createdAt: {
     type: Date,
