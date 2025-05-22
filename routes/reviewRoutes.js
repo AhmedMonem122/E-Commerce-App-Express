4d2d101bde3f0c948f54b58e9e132a37fa23fd22
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-05-22 18:18:41 +0300

    chores: fix update and delete review with the selected user only

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -20,10 +20,12 @@ router
   .get(reviewController.getReview)
   .patch(
     authController.restrictTo("user", "admin"),
+    reviewController.setProductUserIds,
     reviewController.updateReview
   )
   .delete(
     authController.restrictTo("user", "admin"),
+    reviewController.setProductUserIds,
     reviewController.deleteReview
   );
 
